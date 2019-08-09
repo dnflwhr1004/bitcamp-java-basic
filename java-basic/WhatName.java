55b4b0372e96304a2e9745ce29aa565a4e239dbe
dnflwhr1004 <dnflwhr1004@gmail.com>
2019-08-09 16:32:28 +0900

    190809 15:40

--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,8 +1,7 @@
-/**
- * 값을 입력받아서 출력하는 예제 comment(주석)
- */
+
 public class WhatName{
     public static void main(String[] args) {
-        System.out.print("WhatName"); // 출력문 
+        System.out.println("What's your name ?"); 
+        System.out.print("My name is " + "Lee");
     }
 }
